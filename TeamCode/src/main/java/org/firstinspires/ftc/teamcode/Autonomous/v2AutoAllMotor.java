f9ed732d66ea88c995d7f99749048518f3c9b82d
Chungus <45741682+AsianKoala@users.noreply.github.com>
2019-01-01 17:12:06 -0500

    first commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/v2AutoAllMotor.java
@@ -0,0 +1,428 @@
+
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+
+
+@Autonomous(name = "LITPEPEHANDSSS", group = "Working")
+
+public class v2AutoAllMotor extends LinearOpMode {
+
+    private ElapsedTime       runtime = new ElapsedTime();
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+    private DcMotor actuator;
+    private CRServo intake;
+    private DcMotor pulley;
+    private GoldAlignDetector detector;
+
+
+
+    private static final double COUNTS_PER_MOTOR_REV = 1440; //counts per rotation for encoder
+    private static final double DRIVE_GEAR_REDUCTION = 1.0;
+    private static final double ACTUATOR_GEAR_REDUCTION = 1.0;
+    private static final double WHEEL_DIAMETER_INCHES = 4.0;
+    private static final double ACTUATOR_GEAR_DIAMETER = 1.5;
+    private static final double PI = 3.14159265359;
+    private static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * PI);
+    private static final double ACTUATOR_COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * ACTUATOR_GEAR_REDUCTION) / (ACTUATOR_GEAR_DIAMETER * PI);
+
+
+
+
+    @Override
+    public void runOpMode() {
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        actuator = hardwareMap.get(DcMotor.class, "actuator");
+        intake = hardwareMap.get(CRServo.class, "intake");
+        pulley = hardwareMap.get(DcMotor.class, "pulley");
+
+
+        telemetry.addData("Status", "Resetting Encoders");
+        telemetry.update();
+
+
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+
+        telemetry.addData("Status", "DogeCV 2018.0 - Gold Align Example");
+
+        // Set up detector
+        detector = new GoldAlignDetector(); // Create detector
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+
+        // Optional tuning
+        detector.alignSize = 300; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+
+
+        detector.enable(); // Start the detector!
+
+
+        //wait for game to start (driver presses PLAY)
+        waitForStart();
+
+        telemetry.addData("IsAligned", detector.getAligned()); // Is the bot aligned with the gold mineral?
+        telemetry.addData("X Pos", detector.getXPosition()); // Gold X position.
+        telemetry.update();
+
+
+        unlatch();
+
+        mecaDrive(0.25, 6, 10, 5);
+        strafe(0.25, -2.5, 3);
+
+        if (detector.getAligned()) {
+            telemetry.addData("Gold Mineral Aligned!", null);
+            telemetry.update();
+            mecaDrive(0.5, 22, 22, 3);
+
+            detector.disable();
+
+            //pulley.setPower(-1);
+            //intake.setPower(-1);
+            //sleep(750);
+
+            //pulley.setPower(-1);
+            //sleep(250);
+
+            mecaDrive(0.5,-4.5,4.5,5);
+
+            mecaDrive(0.5,-30,-30,10);
+            mecaDrive(0.5,5,-5,5);
+            mecaDrive(0.5,-17.75,-17.75,5);
+            mecaDrive(0.5,10,-10,5);
+            mecaDrive(1,28,28,5);
+
+
+            requestOpModeStop();
+        }
+
+        else {
+            telemetry.addData("Gold Cube Not Aligned", null);
+            telemetry.update();
+
+            strafe(0.5, -9, 5);
+        }
+
+        if (detector.getAligned()) {
+            telemetry.addData("Gold Mineral Aligned!", null);
+            telemetry.update();
+
+            mecaDrive(0.5, 16, 16, 5);
+
+            detector.disable();
+
+            mecaDrive(0.5,-5,5,5);
+            mecaDrive(0.5,11,11,5);
+            mecaDrive(0.5,-32,-32,5);
+            strafe(0.5,4,4);
+            mecaDrive(0.5,4,-4,5);
+            mecaDrive(0.5,-24,-24,10);
+
+
+
+            requestOpModeStop();
+        }
+
+        else {
+            strafe(0.4, 17, 10);
+
+            allMotorsZero();
+            allMotorsResetEncoder();
+
+            mecaDrive(0.5, 10, 10, 6);
+
+            detector.disable();
+
+            strafe(0.75,-4,5);
+            mecaDrive(0.75,6,6,5);
+            mecaDrive(0.3,15,-15,10);
+            mecaDrive(0.5,28,28,10);
+            mecaDrive(0.5,-6,6,5);
+            strafe(0.5,-6,5);
+            mecaDrive(1,15,15,5);
+        }
+    }
+
+
+    public void mecaDrive(double speed, double leftInches, double rightInches, double timeoutS){
+        int newLeftTarget;
+        int newRightTarget;
+
+        if(opModeIsActive())
+        {
+            newLeftTarget = (frontLeft.getCurrentPosition() + backLeft.getCurrentPosition())/2 + (int)(leftInches * COUNTS_PER_INCH);
+            newRightTarget = (frontRight.getCurrentPosition() + backRight.getCurrentPosition())/2 + (int)(rightInches * COUNTS_PER_INCH);
+
+            frontLeft.setTargetPosition(newLeftTarget);
+            backLeft.setTargetPosition(newLeftTarget);
+            frontRight.setTargetPosition(newRightTarget);
+            backRight.setTargetPosition(newRightTarget);
+
+            allMotorsToPosition();
+
+            runtime.reset();
+
+            frontLeft.setPower((Math.abs(speed)));
+            frontRight.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+            while(opModeIsActive() && (runtime.seconds() < timeoutS) &&
+                    (frontLeft.isBusy()) && (frontRight.isBusy()) &&
+                    (backLeft.isBusy()) && (backRight.isBusy()))
+            {
+                telemetry.addData("FrontLeftPower",frontLeft.getPower());
+                telemetry.addData("FrontRightPower",frontRight.getPower());
+                telemetry.addData("BackLeftPower",backLeft.getPower());
+                telemetry.addData("BackRightPower",backRight.getPower());
+                telemetry.update();
+            }
+
+            allMotorsZero();
+            allMotorsResetEncoder();
+            allMotorsRunUsing();
+
+
+        }
+    }
+
+
+    public void strafe(double speed, double FRBLInches, int timeoutS) {
+        int newFRBLTarget;
+        int newFLBRTarget;
+        double FLBRInches = FRBLInches * -1;
+
+        allMotorsResetEncoder();
+
+
+        if(opModeIsActive())
+        {
+            newFRBLTarget = (frontRight.getCurrentPosition() + backLeft.getCurrentPosition())/2 + (int)(FRBLInches * COUNTS_PER_INCH);
+            newFLBRTarget = (frontLeft.getCurrentPosition() + backRight.getCurrentPosition())/2 + (int)(FLBRInches * COUNTS_PER_INCH);
+
+            frontLeft.setTargetPosition(newFLBRTarget);
+            backRight.setTargetPosition(newFLBRTarget);
+            frontRight.setTargetPosition(newFRBLTarget);
+            backLeft.setTargetPosition(newFRBLTarget);
+
+            allMotorsToPosition();
+
+            runtime.reset();
+
+            frontRight.setPower(Math.abs(speed));
+            frontLeft.setPower(Math.abs(speed));
+            backLeft.setPower(Math.abs(speed));
+            backRight.setPower(Math.abs(speed));
+
+                while(opModeIsActive() && (runtime.seconds() < timeoutS)
+                     && (frontLeft.isBusy()) && (frontRight.isBusy()) &&
+                        (backLeft.isBusy()) && (backRight.isBusy()))
+                    {
+                        motorTelemetryPower();
+                    }
+
+            allMotorsZero();
+            allMotorsResetEncoder();
+            allMotorsRunUsing();
+        }
+    }
+
+    public void diagonal(double speed, double Inches, int direction, int timeoutS)
+    {
+        int newFLBRTarget;
+        int newFRBLTarget;
+
+        if(opModeIsActive())
+        {
+            switch (direction)
+            {
+                case 1:
+                    newFLBRTarget = (frontLeft.getCurrentPosition() + backRight.getCurrentPosition())/2 + (int)(Inches * COUNTS_PER_INCH);
+
+                    frontLeft.setTargetPosition(newFLBRTarget);
+                    backRight.setTargetPosition(newFLBRTarget);
+
+                    allMotorsToPosition();
+
+                    runtime.reset();
+
+                    frontLeft.setPower(Math.abs(speed));
+                    backRight.setPower(Math.abs(speed));
+                    frontRight.setPower(-0.01);
+                    backLeft.setPower(-0.01);
+
+                    while(opModeIsActive() && (runtime.seconds() < timeoutS) && (frontLeft.isBusy()) && (backRight.isBusy()))
+                    {
+                        motorTelemetryPower();
+                    }
+
+                    allMotorsZero();
+                    allMotorsRunUsing();
+
+                    break;
+
+                case 2:
+                    newFRBLTarget = (frontRight.getCurrentPosition() + backRight.getCurrentPosition())/2 + (int)(Inches * COUNTS_PER_INCH);
+
+                    frontRight.setTargetPosition(newFRBLTarget);
+                    backLeft.setTargetPosition(newFRBLTarget);
+
+                    allMotorsToPosition();
+
+                    runtime.reset();
+
+                    frontRight.setPower(Math.abs(speed));
+                    backLeft.setPower(Math.abs(speed));
+                    frontLeft.setPower(-0.01);
+                    backRight.setPower(-0.01);
+
+                    while (opModeIsActive() && (runtime.seconds() < timeoutS) && (frontLeft.isBusy()) && (backLeft.isBusy()))
+                    {
+                        motorTelemetryPower();
+                    }
+
+                    allMotorsZero();
+                    allMotorsRunUsing();
+
+                    break;
+            }
+
+        }
+    }
+
+
+
+    private void motorTelemetryPower()
+    {
+        telemetry.addData("FrontLeftPower",frontLeft.getPower());
+        telemetry.addData("FrontRightPower",frontRight.getPower());
+        telemetry.addData("BackLeftPower",backLeft.getPower());
+        telemetry.addData("BackRightPower",backRight.getPower());
+        telemetry.update();
+    }
+
+
+
+    private void allMotorsResetEncoder()
+    {
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+
+
+    private void allMotorsZero()
+    {
+        frontRight.setPower(0);
+        frontLeft.setPower(0);
+        backLeft.setPower(0);
+        backRight.setPower(0);
+    }
+
+
+
+    private void allMotorsToPosition()
+    {
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+
+
+    private void allMotorsRunUsing()
+    {
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+
+
+    private void actuatorMovement(double speed, double inches, double timeoutS){
+
+        int newActuatorTarget;
+
+        if(opModeIsActive())
+        {
+            newActuatorTarget = actuator.getCurrentPosition() + (int)(inches * ACTUATOR_COUNTS_PER_INCH);
+            actuator.setTargetPosition(newActuatorTarget);
+
+            actuator.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            runtime.reset();
+            actuator.setPower(Math.abs(speed));
+
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (actuator.isBusy()))
+            {
+
+                telemetry.addData("Path1","Running to %7d", newActuatorTarget);
+                telemetry.addData("Path2","Running from %7d",
+                        actuator.getCurrentPosition());
+                telemetry.update();
+            }
+
+
+            //Stop all motion
+            actuator.setPower(0);
+
+            //Turn off RUN_TO_POSITION
+            actuator.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+            // sleep(250); //optional pause after each move
+        }
+    }
+
+    private void unlatch()
+    {
+        actuator.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //actuatorMovement(1,-23,10);
+
+        strafe(0.25,3,5);
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/v4MechaTest.java
@@ -0,0 +1,175 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
+//Version 1.4
+
+@TeleOp(name = "I Hope This Works", group = "Working")
+public class v4MechaTest extends LinearOpMode {
+
+    private DcMotor        frontLeft;
+    private DcMotor        frontRight;
+    private DcMotor        backLeft;
+    private DcMotor        backRight;
+    private DcMotor        actuator;
+
+
+
+
+
+    /**
+     * This function is executed when this Op Mode is selected from the Driver Station.
+     */
+
+    @Override
+    public void runOpMode() {
+
+        actuator = hardwareMap.get(DcMotor.class, "actuator");
+        frontLeft  = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft   = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight  = hardwareMap.get(DcMotor.class, "backRight");
+
+
+
+
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+
+
+
+
+        waitForStart();
+        if (opModeIsActive()) {
+        }
+
+        while (opModeIsActive())
+        {
+
+            //either use this with rotation on the right joystick and normal movement on the left joystick
+
+            /*
+
+double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+double rightX = gamepad1.right_stick_x;
+final double v1 = r * Math.cos(robotAngle) + rightX;
+final double v2 = r * Math.sin(robotAngle) - rightX;
+final double v3 = r * Math.sin(robotAngle) + rightX;
+final double v4 = r * Math.cos(robotAngle) - rightX;
+
+frontLeft.setPower(v1);
+frontRight.setPower(v2);
+backLeft.setPower(v3)
+backRight.setPower(v4);
+
+
+*/
+
+
+
+            //or this, which is tank drive with normal joystick and button movement. Also my test code
+
+
+
+
+            frontLeft.setPower((-gamepad1.left_stick_y));
+            backLeft.setPower((-gamepad1.left_stick_y));
+            frontRight.setPower((-gamepad1.right_stick_y));
+            backRight.setPower((-gamepad1.right_stick_y));
+
+            if(gamepad1.left_bumper)
+            {
+                frontLeft.setPower(-1);
+                backRight.setPower(-1);
+                frontRight.setPower(1);
+                backLeft.setPower(1);
+            }
+
+            if(gamepad1.right_bumper)
+            {
+                frontLeft.setPower(1);
+                backRight.setPower(1);
+                frontRight.setPower(-1);
+                backLeft.setPower(-1);
+            }
+
+            if(gamepad1.left_trigger == 1)
+            {
+                frontRight.setPower(1);
+                backLeft.setPower(1);
+                frontLeft.setPower(0);
+                backRight.setPower(0);
+            }
+
+            if(gamepad1.right_trigger == 1)
+            {
+                frontLeft.setPower(1);
+                backRight.setPower(1);
+                frontRight.setPower(0);
+                backLeft.setPower(0);
+            }
+
+            actuator.setPower(gamepad2.left_stick_y);
+
+
+            telemetry.addData("front left power", frontLeft.getPower());
+            telemetry.addData("front right power", frontRight.getPower());
+            telemetry.addData("back left power", backLeft.getPower());
+            telemetry.addData("back right power", backRight.getPower());
+            telemetry.update();
+
+
+
+
+
+
+            /*
+
+            frontLeft.setPower((gamepad1.left_stick_y)/1.5);
+            backLeft.setPower((gamepad1.right_stick_y)/1.5);
+            frontRight.setPower((gamepad2.left_stick_y)/1.5);
+            backRight.setPower((gamepad2.right_stick_y)/1.5);
+
+            */
+
+
+            /*
+
+            double threshold = 0.157;
+
+            if(Math.abs(gamepad1.left_stick_y) > threshold || Math.abs(gamepad1.left_stick_x) > threshold)
+            {
+                frontRight.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x));
+                frontLeft.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x));
+                backRight.setPower((-gamepad1.left_stick_y - gamepad1.left_stick_x));
+                backLeft.setPower((gamepad1.left_stick_y - gamepad1.left_stick_x));
+            }
+
+            if(Math.abs(gamepad1.right_stick_x) > threshold)
+            {
+                frontRight.setPower((-gamepad1.right_stick_x));
+                frontLeft.setPower((-gamepad1.right_stick_x));
+                backLeft.setPower((gamepad1.right_stick_x));
+                backRight.setPower((gamepad1.right_stick_x));
+            }
+
+            */
+
+
+        }
+
+        }
+    }
