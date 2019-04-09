bee0d90a9d014563e927240c23a364adc3913d9d
Chungus <45741682+AsianKoala@users.noreply.github.com>
2019-04-09 17:22:23 -0400

    4/9/19 5:13 PM - ready to program rr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Worlds_Code/Autonomous/roadrunner/opmodes/SplineTestOpMode.java
@@ -0,0 +1,84 @@
+/*
+ *
+ *     Copyright (c) 2018 FTC Team 15167 Robo Troopers (http://robotroopers.org)
+ *
+ *     Permission is hereby granted, free of charge, to any person obtaining a copy
+ *     of this software and associated documentation files (the "Software"), to deal
+ *     in the Software without restriction, including without limitation the rights
+ *     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *     copies of the Software, and to permit persons to whom the Software is
+ *     furnished to do so, subject to the following conditions:
+ *
+ *     The above copyright notice and this permission notice shall be included in all
+ *     copies or substantial portions of the Software.
+ *
+ *     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *     SOFTWARE.
+ *
+ */
+package org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.opmodes;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.drive.SampleMecanumDriveBase;
+import org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.drive.SampleMecanumDriveREVOptimized;
+import org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.drive.SampleMecanumDriveBase;
+import org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.drive.SampleMecanumDriveREV;
+import org.firstinspires.ftc.teamcode.Worlds_Code.Autonomous.roadrunner.util.DashboardUtil;
+
+/*
+ * This is an example of a more complex path to really test the tuning.
+ */
+@Autonomous
+public class SplineTestOpMode extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        SampleMecanumDriveBase drive = new SampleMecanumDriveREVOptimized(hardwareMap);
+
+        Trajectory trajectory = drive.trajectoryBuilder()
+                .splineTo(new Pose2d(30, 30, 0))
+                .waitFor(1)
+                .reverse()
+                .splineTo(new Pose2d(0, 0, 0))
+                .build();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        drive.followTrajectory(trajectory);
+        while (!isStopRequested() && drive.isFollowingTrajectory()) {
+            Pose2d currentPose = drive.getPoseEstimate();
+
+            TelemetryPacket packet = new TelemetryPacket();
+            Canvas fieldOverlay = packet.fieldOverlay();
+
+            packet.put("x", currentPose.getX());
+            packet.put("y", currentPose.getY());
+            packet.put("heading", currentPose.getHeading());
+
+            fieldOverlay.setStrokeWidth(4);
+            fieldOverlay.setStroke("green");
+            DashboardUtil.drawSampledTrajectory(fieldOverlay, trajectory);
+
+            fieldOverlay.setFill("blue");
+            fieldOverlay.fillCircle(currentPose.getX(), currentPose.getY(), 3);
+
+            dashboard.sendTelemetryPacket(packet);
+
+            drive.update();
+        }
+    }
+}
